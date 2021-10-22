33575742d7a047bef084becf40c7d1bac34c9a95
Jon <jon.hogan83@gmail.com>
2021-10-21 22:04:18 -0500

    Add Majaro Script

---
+++ freshManjaro.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+#Get updates
+sudo pacman -Syu -y
+
+#Install C/C++ compiler
+sudo pacman -Sy base-devel -y
+
+#Install and set up Snapd
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+
+sudo ln -s /var/lib/snapd/snap /snap
+
+#install VS Code, Discord, Slack, and Zoom
+sudo snap install code --classic
+sudo snap install Discord
+sudo snap install slack --classic
+sudo snap install zoom-client
+
+#install Java
+sudo pacman -S jdk-openjdk openjdk-src -y
+
+#install Filezilla
+sudo pamac install filezilla -y
