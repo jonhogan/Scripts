d2d7e0ecffb39fa2f17f984247ec8854f7358da9
Jonathan <jon.hogan83@gmail.com>
2022-01-20 20:28:50 -0600

    Re-ordered installs

--- freshUbuntu.sh
+++ freshUbuntu.sh
@@ -16,17 +16,13 @@ sudo apt install filezilla -y
 sudo apt install yakuake -y
 
 #Gnome Keyring
-sudo apt install gnome-keyring
+sudo apt install gnome-keyring -y
 
 #Java Development Kit
-sudo apt install default-jdk
+#sudo apt install default-jdk
 
 #Git - Version Control
 sudo apt install git
-
-#Configure Git
-git config --global user.name "Jon"
-git config --global user.email "jon.hogan83@gmail.com"
 
 #Install Snapd
 sudo apt install snapd  -y
@@ -36,3 +32,7 @@ sudo snap install discord
 sudo snap install code --classic
 sudo snap install slack --classic
 sudo snap install zoom-client
+
+#Configure Git
+git config --global user.name "Jon"
+git config --global user.email "jon.hogan83@gmail.com"
