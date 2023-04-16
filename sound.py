cfc459e4e25486ef92844dc7776204ed74b46c6b
Jon <jon.hogan83@gmail.com>
2023-04-16 00:25:49 -0500

    Updates

--- sound.py
+++ sound.py
@@ -1,36 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
+#Q: Copilot, can you write a recursive function for the fibonacci sequence?
+#A: Sure, here it is:
 
-# Define the URL of the website to scrape
-url = 'https://www.worldometers.info/world-population/population-by-country/'
-
-# Send a request to the website and get the HTML response
-response = requests.get(url)
-html = response.content
-
-# Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(html, 'html.parser')
-
-# Find the table containing the population data
-table = soup.find('table', {'id': 'example2'})
-
-# Create a CSV file and write the headers to it
-with open('population_data.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(['Country', 'State/Province', 'City', 'Population'])
-
-    # Find all the rows in the table and loop through them
-    for row in table.find_all('tr')[1:]:
-        # Extract the country, state/province, city, and population data for the row
-        columns = row.find_all('td')
-        country_name = columns[1].text.strip()
-        state_name = columns[0].text.strip()
-        city_name = columns[2].text.strip()
-        population_data = columns[3].text.strip().replace(',', '')
-
-        # Write the population data to the CSV file
-        writer.writerow([country_name, state_name, city_name, population_data])
-
-# Print a confirmation message to the console
-print('Population data for all countries, states, and cities saved to population_data.csv.')
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+for n in range(10):
+    print(fib(n))
